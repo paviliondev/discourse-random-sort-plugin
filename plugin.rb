8579c1b31c56c22c0506457f5e73cf8125470fb3
Justin DiRose <justin@justindirose.com>
2019-02-04 14:50:15 -0600

    FEATURE: Remove category about topics from randomized list

--- plugin.rb
+++ plugin.rb
@@ -1,6 +1,6 @@
 # name: discourse-random-sort
 # about: Adds a nav item to show random topics.
-# version: 0.1
+# version: 0.2
 # author: Procourse
 # url: https://github.com/discourse/discourse-random-sort-plugin
 
@@ -12,9 +12,11 @@ Discourse.anonymous_filters.push(:random)
 after_initialize do
   require_dependency 'topic_query'
   class ::TopicQuery
-    def list_random
+    def list_random(excluded_topic_ids = [])
+      excluded_topic_ids += Category.topic_ids.to_a
       create_list(:random, unordered: true) do |topics|
-        topics = topics.order("RANDOM()")
+        topics = topics.where("topics.id NOT IN (?)", excluded_topic_ids) unless excluded_topic_ids.empty?
+        topics.order("RANDOM()")
       end
     end
   end
